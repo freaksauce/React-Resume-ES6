0e1ea88d47fc545b7867c449419e86e4bdc92d66
Jon Bloomer <freaksauce@users.noreply.github.com>
2016-10-04 09:23:29 +1100

    initial value should be null for jsonObj

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {jsonObj: props.jsonObj};
+    this.state = {jsonObj: null};
 
     //ajax call to get resume file
     $.get(this.props.source, function(result) {
