672ad771378c3763274a5e9a8fa9df21814bdb43
Jon Bloomer <freaksauce@users.noreply.github.com>
2016-10-06 13:34:00 +1100

    Removed unnecessary imports

--- src/Index.js
+++ src/Index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Profile from './components/Profile';
-import About from './components/About';
-import Work from './components/Work';
-import WorkItem from './components/WorkItem';
-import Skills from './components/Skills';
-import Education from './components/Education';
-
 
 ReactDOM.render(
   <App source="/dist/resume.json" />, document.getElementById('content')
